7ce580ad99041e56f5a01f0678d90a6e7932b586
WenBin Lin <wblin@gmail.com>
2023-02-08 00:04:45 -0500

    add comments

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+// this is the path to your index.html file
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
